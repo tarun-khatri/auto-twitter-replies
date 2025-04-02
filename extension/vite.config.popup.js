083b62acd7d35feb658e84bf523707acd348233a
Tarun Khatri <ttkhatri559@gmail.com>
2025-04-02 19:12:17 +0530

    first commit

---
+++ extension/vite.config.popup.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'src/popup.jsx')
+      },
+      output: {
+        format: 'es',
+        entryFileNames: 'popup.js',
+        chunkFileNames: 'chunks/[name].[hash].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    },
+    outDir: 'dist/popup',
+    minify: false,
+    sourcemap: true,
+    target: 'esnext',
+    cssCodeSplit: false
+  }
+});
